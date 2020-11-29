7c6c4e57649ae6d593232f89c0f976098140852b
cinyplex <63982950+cinyplex@users.noreply.github.com>
2020-11-29 02:17:14 -0800

    update

--- js/presidential_form.js
+++ js/presidential_form.js
@@ -192,3 +192,105 @@ $("#inputGroupSelect12").change(function ()
         }      
     });
 /*=========================Party Names===========================*/
+
+/*=========================Regions & Const ===========================*/
+function autocomplete(searchEle, arr) {
+    var currentFocus;
+    searchEle.addEventListener("input", function(e) {
+       var divCreate,
+       b,
+       i,
+       fieldVal = this.value;
+       closeAllLists();
+       if (!fieldVal) {
+          return false;
+       }
+       currentFocus = -1;
+       divCreate = document.createElement("DIV");
+       divCreate.setAttribute("id", this.id + "autocomplete-list");
+       divCreate.setAttribute("class", "autocomplete-items");
+       this.parentNode.appendChild(divCreate);
+       for (i = 0; i <arr.length; i++) {
+          if ( arr[i].substr(0, fieldVal.length).toUpperCase() == fieldVal.toUpperCase() ) {
+             b = document.createElement("DIV");
+             b.innerHTML = "<strong>" + arr[i].substr(0, fieldVal.length) + "</strong>";
+             b.innerHTML += arr[i].substr(fieldVal.length);
+             b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+             b.addEventListener("click", function(e) {
+                searchEle.value = this.getElementsByTagName("input")[0].value;
+                closeAllLists();
+             });
+             divCreate.appendChild(b);
+          }
+       }
+    });
+    searchEle.addEventListener("keydown", function(e) {
+       var autocompleteList = document.getElementById(
+          this.id + "autocomplete-list"
+       );
+       if (autocompleteList)
+          autocompleteList = autocompleteList.getElementsByTagName("div");
+       if (e.keyCode == 40) {
+          currentFocus++;
+          addActive(autocompleteList);
+       }
+       else if (e.keyCode == 38) {
+          //up
+          currentFocus--;
+          addActive(autocompleteList);
+       }
+       else if (e.keyCode == 13) {
+          e.preventDefault();
+          if (currentFocus > -1) {
+             if (autocompleteList) autocompleteList[currentFocus].click();
+          }
+       }
+    });
+    function addActive(autocompleteList) {
+       if (!autocompleteList) return false;
+          removeActive(autocompleteList);
+       if (currentFocus >= autocompleteList.length) currentFocus = 0;
+       if (currentFocus < 0) currentFocus = autocompleteList.length - 1;
+       autocompleteList[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(autocompleteList) {
+       for (var i = 0; i < autocompleteList.length; i++) {
+          autocompleteList[i].classList.remove("autocomplete-active");
+       }
+    }
+    function closeAllLists(elmnt) {
+       var autocompleteList = document.getElementsByClassName(
+          "autocomplete-items"
+       );
+       for (var i = 0; i < autocompleteList.length; i++) {
+          if (elmnt != autocompleteList[i] && elmnt != searchEle) {
+             autocompleteList[i].parentNode.removeChild(autocompleteList[i]);
+          }
+       }
+    }
+    document.addEventListener("click", function(e) {
+       closeAllLists(e.target);
+    });
+ }
+ var constituencies = 
+ [
+     //Ashanti Region
+    "Adansi-Asokwa","Fomena", "New Edubease", "Afigya Kwabre North ","Afigya Kwabre South ","Ahafo Ano South East","Ahafo Ano South West","Odotobori",
+    "Aminsie West","Manso Nkwanta","Manso Edubia","Asante-Akim North","Asante-Akim Central",
+    "Asante-Akim North ","Asante-Akim South","Asawase ","Atwima Kwanwoma","Atwima-Mponua",
+    "Atwima-Nwabiagya North","Atwima-Nwabiagya South","Bekwa","Bosome-Ferho","Bosomtwe",
+    "Ejisu","Juaben","Ejura","Asokwa ","Mampong","Asokwa","Bantama","Nhyiaeso","Oforikrom",
+    "Old Tafo","Suame", "Subin","Manhyia North", "Manhyia South","Kwadaso","Kwabre East","Akrofuom","Obuasi East","Obuasi West",
+    "Ofinso North","Ofinso South","Kumawu","Sekyere Afram plains","Nsuta-Kwamang Beposo","Afigya Sekyere West","Afigya Sekyere East",
+
+    //Ahafo Region
+    "Asunafo North","Asunafo South","Asutifi North","Tano North","Tano South",
+
+    //Bono Region
+    "Banda  Ahenkro",
+    
+];
+ autocomplete(document.getElementById("constituency"), constituencies);
+
+
+/*=========================PRegions & Const ===========================*/
