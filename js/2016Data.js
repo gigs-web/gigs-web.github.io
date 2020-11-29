169dd1e0bb59b0622f8ac5225db538b0212f97e2
cinyplex <63982950+cinyplex@users.noreply.github.com>
2020-11-29 15:22:00 -0800

    update

--- js/2016Data.js
+++ js/2016Data.js
@@ -2,5 +2,5 @@ $("#data2016").search({
 
 });
 
-$(document).ready(function(){ $('#img1').imageZoom({zoom : 100}); });
-$(document).ready(function(){ $('#img2').imageZoom({zoom : 100}); });
+$(document).ready(function(){ $('#img1').imageZoom({zoom : 200}); });
+$(document).ready(function(){ $('#img2').imageZoom({zoom : 200}); });
